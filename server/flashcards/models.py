cc739edb34a4045d6a1324dc9fcb91b7b75a5ce7
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-22 22:45:40 -0400

    build: create Deck and Flashcard modelsl

--- server/flashcards/models.py
+++ server/flashcards/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Flashcard(models.Model):
+    deck = models.ForeignKey(Deck, related_name='flashcards', on_delete=model.CASCADE)
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+
+    def __str__(self):
+        return self.question
