373a18db119eb34cbd2fdad087f8bfcf8abe9408
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-23 13:32:19 -0400

    chore: update eslint.config.js

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -24,6 +24,7 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      semi: 'off',
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
